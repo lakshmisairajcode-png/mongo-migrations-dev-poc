fc448e8f345b7e9c05a5d3db6d478933e3e5582d
lakshmisairajcode-png <lakshmisairaj.code@gmail.com>
2025-12-09 12:30:16 +0530

    Update migrate-mongo-config.js

--- migrate-mongo-config.js
+++ migrate-mongo-config.js
@@ -1,1 +1,34 @@
 
+const env = process.env.ENV || "dev";
+
+const dbByEnv = {
+  dev: {
+    url: process.env.MONGO_URI_DEV,
+    databaseName: process.env.MONGO_DB_DEV || "CE_DEV_V01",
+  },
+  qa: {
+    url: process.env.MONGO_URI_QA,
+    databaseName: process.env.MONGO_DB_QA || "CE_QA_V01",
+  },
+  prod: {
+    url: process.env.MONGO_URI_PROD,
+    databaseName: process.env.MONGO_DB_PROD || "CE_PROD_V01",
+  },
+};
+
+const current = dbByEnv[env];
+
+const config = {
+  mongodb: {
+    url: current.url,
+    databaseName: current.databaseName,
+    options: {},
+  },
+  migrationsDir: "scripts",
+  changelogCollectionName: "migration_changelog",
+  migrationFileExtension: ".js",
+  useFileHash: false,
+  moduleSystem: "commonjs",
+};
+
+module.exports = config;
